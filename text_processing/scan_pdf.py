6116017ceb0c260fbf7281fcc91ccab37a38a129
ChunxiaZhang <persephonezcx@gmail.com>
2016-02-17 20:13:55 -0500

    added scan pdf files code

---
+++ text_processing/scan_pdf.py
@@ -0,0 +1,21 @@
+import os, os.path
+import re
+
+def print_pdf(root, dirs, files):
+    for file in files:
+        path = os.path.join(root, file)
+        path = os.path.normcase(path)   #lowercase
+        if not re.search(r".*\.pdf", path): continue
+        if re.search(r".*\.pdf", path):
+            print(path)
+            
+for root, dirs, files in os.walk('.'):
+    print_pdf(root, dirs, files)              
+            
+            
+            
+"""
+Regular expression:
+1. '.' any one character in a string. 'x.x' will match the strings 'xxx', 'xyx', 'x.x';
+2. r"x\.x" matches 'x.x'
+"""            
